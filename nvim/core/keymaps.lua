57c8402cf69c10e3126c95e2a534623134d85066
maksimzhohov <zhohovmaksim@gmail.com>
2024-07-10 02:48:14 +0300

    feat(keymaps): Disable arrow keys and add keymap for open neotree

--- nvim/core/keymaps.lua
+++ nvim/core/keymaps.lua
@@ -1,7 +1,14 @@
+-- Map <Esc> to jk
 vim.keymap.set('i', 'jk', '<Esc>')
 
--- NvimTree
-vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>')
+-- Disable arrow keys
+vim.keymap.set('n', '<up>', '<nop>')
+vim.keymap.set('n', '<down>', '<nop>')
+vim.keymap.set('n', '<left>', '<nop>')
+vim.keymap.set('n', '<right>', '<nop>')
+
+-- NeoTree
+vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>')
 
 -- Bufferline
 vim.keymap.set('n', '<leader>bl', ':BufferLinePick<CR>')
@@ -12,3 +19,4 @@ vim.keymap.set('n', '<Tab>', ':BufferLineCycleNext<CR>')
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
